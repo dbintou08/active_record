d4e5ceca6b5bca14afa03f0be83a614d26bf1984
dpro <dbintou008@gmail.com>
2023-04-25 17:44:01 +0200

    testing

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -24,6 +24,7 @@ class ExercisesController < ApplicationController
     .select("addresses.*, COUNT(orders.*) orders_count")
     .group("addresses.id")
     .order("orders_count DESC")
+    .first
   end
 
   def exercise4 
@@ -35,5 +36,6 @@ class ExercisesController < ApplicationController
     .select("customers.*, SUM(foods.price) AS foods_price_sum")
     .group("customers.id")
     .order("foods_price_sum DESC")
+    .first
   end
 end
